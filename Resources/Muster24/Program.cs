23d4ea47d201cf3f2ff1fb7b344e4e47d4aad155
Valacor <valyrious.informatics@gmail.com>
2024-05-03 19:59:35 +0200

    Adding some excerises

---
+++ Resources/Muster24/Program.cs
@@ -0,0 +1,17 @@
+﻿
+internal static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Geben sie die Iterationsnummer ein: ");
+        int n = int.Parse(Console.ReadLine());
+        int summe = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            summe += i;
+        }
+
+        Console.WriteLine(summe);
+    }
+}
